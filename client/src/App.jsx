7b94a760d9f230c92a67bb5c6bf5e6a14adf8f0e
siddharthswami23 <siddharth.swami@walchandsangli.ac.in>
2024-12-28 13:26:00 +0530

    NavBar and useState

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from "react";
+import Home from "./Pages/Home";
+import BuyCredit from "./Pages/BuyCredit";
+import Result from "./Pages/Result";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      app
+    <div className="w-full min-h-screen px-28 bg-gradient-to-b from-teal-50 to-orange-50">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/result" element={<Result />} />
+        <Route path="/buy" element={<BuyCredit />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
